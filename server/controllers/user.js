b3cfdfb4b41b4da0b2e45dba3fb302295825a2be
Vilmar Rodrigues Martins <vilmar.rodrigs@gmail.com>
2019-03-30 14:22:50 -0300

    feat(test): Created user endpoint to test

---
+++ server/controllers/user.js
@@ -0,0 +1,31 @@
+// import UserModel from '../models/user';
+
+const UserController = {
+  get: (req, res) => {
+    res
+      .status(200)
+      .json(req.param);
+  },
+  post: (req, res) => {
+    res
+      .status(201)
+      .json(req.body);
+  },
+  put: (req, res) => {
+    res
+      .status(201)
+      .json(req.body);
+  },
+  getById: (req, res) => {
+    res
+      .status(200)
+      .json(req.param);
+  },
+  deleteById: (req, res) => {
+    res
+      .status(200)
+      .json(req.param);
+  }
+};
+
+export default UserController;
