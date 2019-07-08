a6b3c4e08c334037116d793d4246434729356f19
Vilmar Rodrigues Martins <vilmar.rodrigs@gmail.com>
2019-07-08 20:07:25 -0300

    chore: Removed mongoose warning

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const databaseUri = function() {
   return `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_ADDRESS}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
 }();
 
-mongoose.connect(databaseUri, {useNewUrlParser: true});
+mongoose.connect(databaseUri, {useNewUrlParser: true, useCreateIndex: true});
 
 app.use(helmet());
 
